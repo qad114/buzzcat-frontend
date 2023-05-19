1abfca49a577b72db9b6082bf6d6d6bc7fbd990b
Hamza Qadri <syedhamzaq@gmail.com>
2023-05-19 09:36:58 -0400

    Rewrote Tabs component to use own state

--- src/components/reusable/Tabs/Tabs.js
+++ src/components/reusable/Tabs/Tabs.js
@@ -1,19 +1,52 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { ThemeContext } from '../../../contexts/ThemeContext';
 import css from './Tabs.module.css';
 
-export default function Tabs({ className, state }) {
+export default function Tabs({ className, views }) {
   const { theme } = useContext(ThemeContext);
-  const [tabs, setTabs] = state;
+  const [tabIndex, setTabIndex] = useState(0);
 
-  function onTabClick(clickedName) {
-    setTabs(tabs.map(([tabName, isActive]) => [tabName, tabName === clickedName]));
-  }
-
-  console.log(tabs);
   return (
     <div className={[css.Tabs, className].join(' ')}>
-      {tabs.map(([tabName, isActive]) => <button key={tabName} className={[theme, isActive ? css.active : css.inactive].join(' ')} onClick={() => {onTabClick(tabName);}}>{tabName}</button>)}
+      <div className={[css.tabContainer, 'Tabs__tabContainer', theme, 'bg-5'].join(' ')}>
+        {views.map(([tabName, tabView], index) =>
+          <button key={index} className={[theme, tabIndex === index ? css.active : css.inactive].join(' ')} onClick={() => setTabIndex(index)}>
+            {tabName}
+          </button>
+        )}
+      </div>
+      <div className={[css.viewContainer, 'Tabs__viewContainer', theme].join(' ')}>
+        {views[tabIndex][1]}
+      </div>
     </div>
   )
 }
+
+/*
+import { useContext, useState } from 'react';
+import { ThemeContext } from '../../../contexts/ThemeContext';
+import css from './Tabs.module.css';
+
+const views = {
+  "Overview": <div></div>
+}
+
+export default function Tabs({ className, views }) {
+  const { theme } = useContext(ThemeContext);
+  const [tabIndex, setTabIndex] = useState(0);
+  return (
+    <div className={[css.Tabs, className].join()}>
+      <div className={css.tabs}>
+        {views.map(([tabIndex, tabView], index) => 
+          <button key={index} className={[theme, tabIndex === index ? css.active : css.inactive].join(' ')} onClick={() => setTabIndex(index)}>
+            {views[index][0]}
+          </button>
+        )}
+      </div>
+      <div className={css.viewContainer}>
+        {views[tabIndex][1]}
+      </div>
+    </div>
+  )
+}
+*/
