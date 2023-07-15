55940f1d1014ed55ee0e85b8006c09b3a3e8fd55
Hamza Qadri <syedhamzaq@gmail.com>
2023-07-15 19:25:52 +0100

    Change ListItem from div to button

--- src/components/reusable/ListItem.tsx
+++ src/components/reusable/ListItem.tsx
@@ -3,13 +3,13 @@ import css from '#src/styles/ListItem.module.scss';
 export default function ListItem({ className = '', tags = [], warningTags = [], mainText, subText, onClick }:
     {className?: string, tags?: (string | undefined)[], warningTags?: (string | undefined)[], mainText: string, subText?: string | JSX.Element, onClick?: () => void}) {
   return (
-    <div className={[ListItem.name, css.root, className].join(' ')} onClick={onClick}>
+    <button className={[ListItem.name, css.root, className].join(' ')} onClick={onClick}>
       <div className={css.row}>
         {tags.map(tagText => tagText === undefined ? null : <div className={css.tag}>{tagText}</div>)}
         {warningTags.map(tagText => tagText === undefined ? null : <div className={[css.tag, css.warning].join(' ')}>{tagText}</div>)}
         <div className={css.mainText}>{mainText}</div>
       </div>
       {subText === undefined ? <></> : <div className={css.subText}>{subText}</div>}
-    </div>
+    </button>
   )
 }
